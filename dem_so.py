37ba3a2b609d5ece39f9fe6142fa4f40dbcc4088
nguyetnong <nguyetnong71099@gmail.com>
2022-11-26 16:53:32 +0700

    Dem so

---
+++ dem_so.py
@@ -0,0 +1,9 @@
+
+def count_char(chuoi):
+    cnt=0
+    for i in (chuoi):
+        cnt+=1
+    return cnt
+    
+chuoi=(input("nhap chuoi can dem so ki tu:"))
+print("so ki tu cua chuoi la:",count_char(chuoi))
